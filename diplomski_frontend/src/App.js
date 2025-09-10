38df8c3c08edf270bf160e9671d95fc8722ee5f6
Dora Smaila <dora.smaila8@gmail.com>
2025-09-10 16:20:00 +0200

    Footer

--- diplomski_frontend/src/App.js
+++ diplomski_frontend/src/App.js
@@ -11,25 +11,25 @@ import Footer from './components/Footer';
 const AppContent = () => {
   const location = useLocation();
   const showNavbar = location.pathname !== '/';
-  const isAuthPage = location.pathname === '/';
 
   return (
-    <div className={`app-container ${isAuthPage ? 'auth-page' : ''}`}>
+    <>
       {showNavbar && <Navbar />}
-      <div className="app-content">
-        <Routes>
-          <Route path="/" element={<AuthPage />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/property/:id" element={<PropertyDetails />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/analytics" element={<Analytics />} />
-        </Routes>
-      </div>
+
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/analytics" element={<Analytics />} />
+      </Routes>
+
       <Footer />
-    </div>
+    </>
   );
 };
+
 
 
 
